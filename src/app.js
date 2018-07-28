909d370d759e96d5c452be2cc449e54390ce9ff1
americanmouths <shiverr@gmail.com>
2018-07-28 14:26:28 -0700

    build out POST request to create new post

--- src/app.js
+++ src/app.js
@@ -4,9 +4,34 @@ import { ui } from './ui';
 //Get posts on DOM load 
 document.addEventListener('DOMContentLoaded', getPosts);
 
+//Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
 //Get posts function
 function getPosts(){
   http.get('http://localhost:3000/posts')
     .then(posts => ui.showPosts(posts))
     .catch(err => console.log(err))
+}
+
+//Add new post function
+
+function submitPost(){
+  const title = document.getElementById('title').value;
+  const body = document.getElementById('body').value;
+
+  const data = {
+    title: title,
+    body: body
+  }
+
+  //Make POST req
+  http.post('http://localhost:3000/posts', data)
+    .then(post => {
+      ui.showAlert('Post added!', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
+    .catch(err => console.log(err));
+
 }
